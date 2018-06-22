9d5639d956dfb7f47ccc1fe71735d24216b27466
Guillermo Fink (CM) <guillermo.fink@ar.vision101.corp>
2018-06-22 16:14:03 -0300

    crear jwt con login en usuario

--- clases/usuario.php
+++ clases/usuario.php
@@ -1,98 +1,28 @@
 <?php
+class Usuario{
 
-class usuario {
+    public $id;
+    public $nombre;
+    public $apellido;
+    public $mail;
+    public $password;
+    public $tipo;
 
-    private $_id_usuario;
-    private $_mail;
-    private $_password;
-    private $_nombre;
-    private $_apellido;
-    private $_dni;
-    private $_telefono;
-    private $_tipo;
 
-    //AGREGAR usuario
-    public static function agregarUsuario($mail,$password,$nombre,$apellido,$dni,$telefono,$tipo)
-    {
-        $rta = false;
+    public static function Login($mail, $password){
+        $rta = "error";
         $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
-        $consulta =$objetoAccesoDato->RetornarConsulta("INSERT into  
-        usuarios (mail,password,nombre,apellido,dni,telefono,tipo)
-        values(:mail,:password,:nombre,:apellido,:dni,:telefono,:tipo)");
-
+        $consulta = $objetoAccesoDato->RetornarConsulta("SELECT * FROM usuarios WHERE mail=:mail AND password=:password");
         $consulta->bindValue(':mail',$mail);
         $consulta->bindValue(':password', $password);
-        $consulta->bindValue(':nombre',$nombre);
-        $consulta->bindValue(':apellido', $apellido);
-        $consulta->bindValue(':dni', $dni);
-        $consulta->bindValue(':telefono',$telefono);
-        $consulta->bindValue(':tipo',$tipo);
-        
-        if($consulta->execute()){
-            $rta = $objetoAccesoDato->RetornarUltimoIdInsertado();
-        }
-        return $rta; 
-    }    
-
-    //TRAER TODOS LOS usuarios
-    public static function traerTodosLosUsuarios()
-    {
-        $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso();
-        $consulta = $objetoAccesoDato->RetornarConsulta("SELECT * FROM usuarios");
-        $consulta->execute();
-        $consulta = $consulta->fetchAll(PDO::FETCH_ASSOC);
-        return json_encode($consulta);
-    }
-
-    //TRAER usuario POR ID
-    public static function traerUsuarioPorId($id)
-    {
-        $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso();
-        $consulta = $objetoAccesoDato->RetornarConsulta("SELECT * FROM usuarios WHERE id_usuario=:id");
-        $consulta->bindValue(":id",$id);
-        $consulta->execute();
-        $datos = $consulta->fetchAll(PDO::FETCH_ASSOC);
-        return json_encode($datos);     
-    }
-
-   //MODIFICAR usuario
-    public static function modificarUsuario($id,$mail,$password,$nombre,$apellido,$dni,$telefono,$tipo){
-        $rta = false;
-        $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso();
-        $consulta = $objetoAccesoDato->RetornarConsulta("UPDATE `usuarios` 
-        SET `mail`= :mail,
-        `password`= :password,
-        `nombre`= :nombre,
-        `apellido`=:apellido,
-        `dni`= :dni, 
-        `telefono`= :telefono,
-        `tipo`= :tipo
-        WHERE id_usuario = :id");
-
-        $consulta->bindValue(':id',$id);
-        $consulta->bindValue(':mail',$mail);
-        $consulta->bindValue(':password', $password);
-        $consulta->bindValue(':nombre',$nombre);
-        $consulta->bindValue(':apellido', $apellido);
-        $consulta->bindValue(':dni', $dni);
-        $consulta->bindValue(':telefono',$telefono);
-        $consulta->bindValue(':tipo',$tipo);
-
         if ($consulta->execute()){
-            $rta = true;
+            $datos = $consulta->fetchAll(PDO::FETCH_ASSOC);
+            if (isset($datos[0]['nombre'])){
+                $datos=json_encode($datos); 
+                return AutentificadorJWT::CrearToken($datos);
+            }
         }
         return $rta;
     }
-
-    //BORRAR usuario
-    public static function borrarUsuario($id){
-        $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso();
-        $consulta = $objetoAccesoDato->RetornarConsulta("DELETE FROM usuarios WHERE id_usuario=:id");
-        $consulta->bindValue(":id",$id);
-        $consulta->execute();
-        $datos = $consulta->fetchAll(PDO::FETCH_ASSOC);
-        return json_encode($datos);     
-    }
- 
 }
 ?>
